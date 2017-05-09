3dcb3b0231fadc557db0b516addbaac00a67739a
Stefan <santonrod@gmail.com>
2017-05-08 21:42:26 -0500

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 console.log('Hello World!');
+import React from 'react';
+import ReactDom from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  React.dom = render(
+    React.createElement(Counter)
+    document.getElementById('mount')
+  );
+});

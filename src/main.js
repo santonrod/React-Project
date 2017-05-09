e2d72910a925f1087671cb3e084a467bdb38ab58
Stefan <santonrod@gmail.com>
2017-05-08 22:13:36 -0500

    add react and fix version issues to run server

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 console.log('Hello World!');
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import Counter from './Counter';
-
+ 
 document.addEventListener('DOMContentLoaded', function() {
-  React.dom = render(
-    React.createElement(Counter)
+  ReactDOM.render(
+    React.createElement(Counter),
     document.getElementById('mount')
   );
 });
